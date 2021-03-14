2d671828d5989ca18523a1dba36783596619688a
Ian Cleasby <3924194+PenguinRage@users.noreply.github.com>
2021-03-15 03:01:35 +1100

    Locating the required gigapixels to render...

--- main.py
+++ main.py
@@ -18,22 +18,30 @@ option.binary_location = brave_path
 # option.add_argument("--headless")
 
 
+class pizza(Enum):
+    MEATLOVERS = '//*[@id="menu"]/span/div[11]/ul[2]/li[9]/a/div[2]/h3'
+    PERIPERI = '//*[@id="menu"]/span/div[11]/ul[2]/li[34]/a/div[2]/h3'
+    PEPPERONI = '//*[@id="menu"]/span/div[11]/ul[2]/li[15]/a/div[2]/h3'
+    SPICYCHICKEN = '//*[@id="menu"]/span/div[11]/ul[2]/li[16]/a/div[2]/h3'
+
+
 class meal(Enum):
-    FAMILY = '//*[@id="meal-combos"]/ul/li[3]/a/div/span'
     ME = '//*[@id="meal-combos"]/ul/li[2]/a/div/span'
+    FAMILY = '//*[@id="meal-combos"]/ul/li[3]/a/div/span'
 
 
 def fill_address_details(driver):
     driver.get('https://orders.eatapp.online/menu/golden-crust-baulkham-hills#ordering-for-prompt')
     try:
         driver.find_element_by_xpath('//*[@id="start-order-type"]/div[1]/div[2]/button[3]').click()
-        driver.find_element_by_xpath('//*[@id="start-delivery-address"]').send_keys("123 Fake Drive")
-        driver.find_element_by_xpath('//*[@id="start-delivery-address-line2"]').send_keys("Seven Hills")
-        driver.find_element_by_xpath('//*[@id="start-delivery-zip"]').send_keys("1234")
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="start-delivery-address"]').send_keys("19 Allandale Drive")
+        driver.find_element_by_xpath('//*[@id="start-delivery-address-line2"]').send_keys("Baulkham Hills")
+        driver.find_element_by_xpath('//*[@id="start-delivery-zip"]').send_keys("2153")
         driver.find_element_by_xpath('//*[@id="start-order-time-info"]/div[2]/button[1]').click()
         driver.find_element_by_xpath('//*[@id="start-order"]').click()
     except ElementClickInterceptedException:
-        print(driver.find_element_by_xpath('//*[@id="start-order-time-info"]/div[3]').text)
+        print("Exception:" + driver.find_element_by_xpath('//*[@id="start-order-time-info"]/div[3]').text)
         exit(1)
     time.sleep(5)
 
@@ -41,7 +49,10 @@ def fill_address_details(driver):
 def get_order(driver):
     driver.find_element_by_link_text('Meal Combos').click()
     time.sleep(1)
-    driver.find_element_by_xpath(meal.FAMILY.value).click()
+    driver.find_element_by_xpath(meal.ME.value).click()
+    time.sleep(1)
+    driver.find_element_by_xpath(pizza.MEATLOVERS.value).click()
+
 
 
 driver = webdriver.Chrome(options=option, executable_path=driver_path)
