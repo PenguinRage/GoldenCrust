a62f2a2df0c071a9d278e4817c4bed44d21e3429
Ian Cleasby <3924194+PenguinRage@users.noreply.github.com>
2021-04-04 19:26:44 +1000

    happy monday _ bleh _

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException, \
+    ElementNotInteractableException
 
 import time
 from enum import Enum
 # Check version in Brave: brave://version/
 # Chromedriver location: https://sites.google.com/a/chromium.org/chromedriver/
 # we need to freeze this.......
-from selenium.webdriver.support import expected_conditions, wait
 
 # Browser config
+from selenium.webdriver.support.wait import WebDriverWait
+
 driver_path = "./chromedriver"
 brave_path = "/usr/lib/brave/brave"
 option = Options()
@@ -18,6 +21,9 @@ option.binary_location = brave_path
 # option.add_argument("--headless")
 
 
+# Todo: Move this logic else where. Responsibility could be given to a builder
+# Todo: Secure credentials/home details elsewhere
+# TODO: Fix website is drastically changed on this day, logic for ordering a deal is different
 class pizza(Enum):
     MEATLOVERS = '//*[@id="menu"]/span/div[11]/ul[2]/li[9]/a/div[2]/h3'
     PERIPERI = '//*[@id="menu"]/span/div[11]/ul[2]/li[34]/a/div[2]/h3'
@@ -30,7 +36,15 @@ class meal(Enum):
     FAMILY = '//*[@id="meal-combos"]/ul/li[3]/a/div/span'
 
 
-def fill_address_details(driver):
+class sides(Enum):
+    GARLICBREAD = '//*[@id="menu"]/span/div[11]/ul[2]/li[1]/a/div[2]'
+
+
+class drinks(Enum):
+    PEPSIMAX = '//*[@id="menu"]/span/div[11]/ul[2]/li[4]/a'
+
+
+def fill_address_details():
     driver.get('https://orders.eatapp.online/menu/golden-crust-baulkham-hills#ordering-for-prompt')
     try:
         driver.find_element_by_xpath('//*[@id="start-order-type"]/div[1]/div[2]/button[3]').click()
@@ -46,18 +60,37 @@ def fill_address_details(driver):
     time.sleep(5)
 
 
-def get_order(driver):
-    driver.find_element_by_link_text('Meal Combos').click()
+def get_order():
+    try:
+        driver.find_element_by_link_text('Meal Combos').click()
+    except NoSuchElementException:
+        print("KNOWN BUG: Tuesdays Special offerings break here")
+        driver.close()
+        exit(2)
     time.sleep(1)
     driver.find_element_by_xpath(meal.ME.value).click()
-    time.sleep(1)
-    driver.find_element_by_xpath(pizza.MEATLOVERS.value).click()
+    add_to_order(pizza.MEATLOVERS)
+    add_to_order(sides.GARLICBREAD)
+    add_to_order(drinks.PEPSIMAX)
 
+
+def add_to_order(item):
+    time.sleep(5)
+    element = WebDriverWait(driver, 10).until(
+        lambda finder: finder.find_element_by_xpath(item.value))
+    element.click()
+    time.sleep(5)
+    try:
+        element = WebDriverWait(driver, 10).until(
+            lambda finder: finder.find_element_by_xpath('//*[@id="menu-app"]/div[13]/div/div/form/div[2]/button[3]'))
+        element.click()
+    except ElementNotInteractableException:
+        element = WebDriverWait(driver, 10).until(
+            lambda finder: finder.find_element_by_xpath('//*[@id="menu-app"]/div[13]/div/div/form/div[2]/button[1]/span'))
+        element.click()
 
 
 driver = webdriver.Chrome(options=option, executable_path=driver_path)
-fill_address_details(driver)
-get_order(driver)
-
-
+fill_address_details()
+get_order()
 
