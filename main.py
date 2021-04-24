8a52e0cb4fc61ed3f052a05443db1386efb5a2a1
Ian Cleasby <3924194+PenguinRage@users.noreply.github.com>
2021-04-24 13:43:28 +1000

    added super-widget 2.0.

--- main.py
+++ main.py
@@ -107,6 +107,7 @@ def complete_order():
 
     driver.find_element_by_xpath('//*[@id="month"]').send_keys(db.get("exp_month"))
     driver.find_element_by_xpath('//*[@id="year"]').send_keys(db.get("exp_year"))
+    #select_button_by_link_text("Submit Order")
 
 
 def select_button_by_link_text(text):
