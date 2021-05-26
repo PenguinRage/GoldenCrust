c0d599fcd6b1f37e693ba189bfc14eb297e003c9
Ian Cleasby <3924194+PenguinRage@users.noreply.github.com>
2021-05-26 22:07:36 +1000

    I immediately regret this commit.

--- app/dao/bitwardenDAO.py
+++ app/dao/bitwardenDAO.py
@@ -1,11 +1,16 @@
 import json
-import os
 import subprocess
 
 
 class BitwardenDAO:
+    SESSION = None
 
-    @staticmethod
-    def get_item(topic):
-        result = subprocess.run(['bw', 'get', 'item', topic, '--session', os.environ['BW_SESSION']], stdout=subprocess.PIPE)
+    def __init__(self):
+        result = subprocess.run(['bw', 'unlock'],
+                                stdout=subprocess.PIPE)
+        self.SESSION = str(result.stdout.split()[-1], 'utf-8')
+
+    def get_item(self, topic):
+        result = subprocess.run(['bw', 'get', 'item', topic, '--session', self.SESSION],
+                                stdout=subprocess.PIPE)
         return json.loads(result.stdout)
